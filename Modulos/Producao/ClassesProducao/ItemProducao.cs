343cfba287ff33ce2990b5e5e4c661c5c0a06167
veronezzi159 <lucasveronezzi3@gmail.com>
2024-05-09 15:37:22 -0300

    commit classes

---
+++ Modulos/Producao/ClassesProducao/ItemProducao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5by5_Biltiful.Modulos.Producao.ClassesProducao
+{
+    internal class ItemProducao
+    {
+        int Id;
+        DateOnly DataProducao;
+        string MateriaPrima;
+        int QuantidadeMateriaPrima;
+
+    }
+}
---
+++ Modulos/Producao/ClassesProducao/Producao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5by5_Biltiful.Modulos.Producao.ClassesProducao
+{
+    internal class Producao
+    {
+        int Id;
+        DateOnly DataProducao;
+        string Produto;
+        int Quantidade;              
+
+    }
+}
