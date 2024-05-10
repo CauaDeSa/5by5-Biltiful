e411927f4143f6d324bd8cb567729ad7d4e2c816
JulianaMistron <jumistron@yahoo.com.br>
2024-05-10 14:55:41 -0300

    Alteração módulo de vendas

---
+++ Modulos/Venda/ClassesVenda/ItemVendaProduto.cs
@@ -0,0 +1,36 @@
+﻿namespace _5by5_Biltiful.Modulos.Venda
+{
+    internal class ItemVendaProduto
+    {
+        public int IdVenda { get; set; }             //5  (0-4)
+        public string Produto { get; set; } //é o código de barras do produto  - 13 (5-17)
+        public int Quantidade { get; set; }             //3 (18-20)
+        public int ValorUnitário { get; set; }          //5  (21-25)
+        public int ValorTotal { get; set; }             //6  (26-31)
+
+        public ItemVendaProduto(int idVenda, string produto, int quantidade, int valorUnitário, int valorTotal)
+        {
+            IdVenda = idVenda;
+            Produto = produto;
+            Quantidade = quantidade;
+            ValorUnitário = valorUnitário;
+            ValorTotal = valorTotal;
+        }
+        public ItemVendaProduto(string data)
+        {
+            IdVenda = int.Parse(data.Substring(0, 5));
+            Produto = data.Substring(5, 17);
+            Quantidade = int.Parse(data.Substring(3, 20));
+            ValorUnitário = int.Parse(data.Substring(5, 25));
+            ValorTotal = int.Parse(data.Substring(6, 31));
+        }
+        private DateOnly ConverterParaData(string data)
+        {
+            return DateOnly.ParseExact(data, "ddMMyyyy", null);
+        }
+        public string FormatarParaArquivoVenda()
+        {
+            return IdVenda + Produto + Quantidade + ValorUnitário + ValorTotal;
+        }
+    }
+}
--- Modulos/Venda/ModuloVenda.cs
+++ Modulos/Venda/ModuloVenda.cs
@@ -1,49 +1,59 @@
-﻿namespace biltiful.Modulos
+﻿using System.ComponentModel;
+
+namespace biltiful.Modulos
 {
     internal class ModuloVenda
     {
 
         void CadastrarVenda()
         {
-            Console.WriteLine("pega os dados para cadastro da venda");
+            Console.WriteLine(">>>CADASTRAR VENDA<<<");
+            Console.WriteLine("Informe o CPF do cliente: ");
+            int cpf = int.Parse(Console.ReadLine());
+
+            //imprime na tela o nome e data de nascimento do cliente para fazer uma confirmação
+            //e verificar se não esta bloqueado ou é menor de idade
+
             Console.WriteLine("faz trativas");
             Console.WriteLine("salva no arquivo tudo que for necessario");
         }
 
         void LocalizarVenda()
         {
+            Console.WriteLine(">>>LOCALIZAR VENDA<<<");
             Console.WriteLine("pega os dados para localizar venda");
             Console.WriteLine("faz trativas");
             Console.WriteLine("le do arquivo tudo que for necessario");
         }
 
+        void ExibirVenda()
+        {
+            Console.WriteLine(">>>EXIBIR VENDA<<<");
+            Console.WriteLine("pega os dados para exibir venda");
+            Console.WriteLine("faz trativas");
+            Console.WriteLine("le do arquivo e exibe venda");
+        }
         void ExcluirVenda()
         {
+            Console.WriteLine(">>>EXCLUIR VENDA<<<");
             Console.WriteLine("pega os dados para excluir venda");
             Console.WriteLine("faz trativas");
             Console.WriteLine("exclui venda");
         }
 
-        void ExibirVenda()
-        {
-            Console.WriteLine("pega os dados para exibir venda");
-            Console.WriteLine("faz trativas");
-            Console.WriteLine("le do arquivo e exibe venda");
-        }
-
         public void Executar()
         {
             Console.Clear();
-            Console.WriteLine("[ 1 ] Cadastrar venda");
-            Console.WriteLine("[ 2 ] Localizar venda");
-            Console.WriteLine("[ 3 ] Excluir venda");
-            Console.WriteLine("[ 4 ] Exibir venda");
+            Console.WriteLine("[ 1 ] CADASTRAR VENDA");
+            Console.WriteLine("[ 2 ] LOCALIZAR VENDA");
+            Console.WriteLine("[ 3 ] EXIBIR VENDA");
+            Console.WriteLine("[ 4 ] EXCLUIR VENDA");
 
             int opcao = int.Parse(Console.ReadLine());
 
             while (opcao < 1 || opcao > 4)
             {
-                Console.Write("Opcao invalida, tente novamente: ");
+                Console.Write("Opcao inválida, tente novamente: ");
                 opcao = int.Parse(Console.ReadLine());
             }
 
@@ -56,10 +66,13 @@
                     LocalizarVenda();
                     break;
                 case 3:
+                    ExibirVenda();
+                    break;
+                case 4:
                     ExcluirVenda();
                     break;
                 default:
-                    ExibirVenda();
+                    Environment.Exit(0);
                     break;
             }
         }
