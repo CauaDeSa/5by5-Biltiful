dd0604fb6cb844e67d747475c0465c2932fdd375
Cauã Rufino de Sá <CauaRufinodeSa@Gmail.com>
2024-05-10 20:14:00 -0300

    feat Classes de validações implementadas.

--- Modulos/Cadastro/ClassesCadastro/Validacao/ValidarCB.cs
+++ Modulos/Cadastro/ClassesCadastro/Validacao/ValidarCB.cs
@@ -8,6 +8,28 @@ namespace _5by5_Biltiful.Modulos.Cadastro.ClassesCadastro.Validacao
 {
     public static class ValidarCB
     {
-        
+        public static bool VerificarCB(string cb)
+        {
+            if (cb.Length != 13)
+                return false;
+
+            if (!int.TryParse(cb.Substring(0, 3), out _))
+                return false;
+
+            return ValidarDV(cb);
+        }
+
+        private static bool ValidarDV(string cb)
+        {
+            int result = 0;
+
+            for (int i = 0; i < cb.Length; i += 2)
+                result += int.Parse(cb.Substring(i, 1));
+
+            for (int i = 1; i < cb.Length; i += 2)
+                result += int.Parse(cb.Substring(i, 1)) * 3;
+
+            return 10 - (result % 10);
+        }   
     }
 }
--- Modulos/Cadastro/ClassesCadastro/Validacao/ValidarCNPJ.cs
+++ Modulos/Cadastro/ClassesCadastro/Validacao/ValidarCNPJ.cs
@@ -6,8 +6,36 @@ using System.Threading.Tasks;
 
 namespace _5by5_Biltiful.Modulos.Cadastro.ClassesCadastro.Validacao
 {
-    internal class ValidarCNPJ
+    public static class ValidarCNPJ
     {
+        public static bool VerificarCNPJ(string cnpj)
+        {
+            if (cnpj.Length != 14)
+                return false;
 
+            if (!int.TryParse(cnpj, out _))
+                return false;
+
+            if (cnpj.Count(value => value == cnpj[0]) > cnpj.Length - 1)
+                return false;
+
+            return ValidarDV(cnpj, 1) && ValidarDV(cnpj, 2);
+        }
+
+        private static bool ValidarDV(string cpf, int dv)
+        {
+            int result = 0;
+
+            for (int posicao = 0, multiplicador = 4 + dv; posicao < 12 ; posicao++, multiplicador--)
+            {
+                if (multiplicador == 1)
+                    multiplicador = 9;
+                result += int.Parse(cpf.Substring(posicao, 1)) * multiplicador;
+            }
+
+            result %= 11;
+
+            return (result == 0 || result == 1 ? 0 : 11 - result) == int.Parse(cpf.Substring(11 + dv, 1));
+        }
     }
 }
